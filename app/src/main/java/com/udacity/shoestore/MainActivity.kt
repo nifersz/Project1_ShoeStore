5ae49330568f28929be885054898e994f5324ca5
Nelson Fernandez <nifersz@icloud.com>
2021-05-19 09:36:00 -0500

    Create project using Android Studio IDE(using starter project code)

---
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.ActivityMainBinding
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(
+            this, R.layout.activity_main
+        )
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        Timber.plant(Timber.DebugTree())
+    }
+
+}
---
+++ app/src/main/java/com/udacity/shoestore/ui/LoginFragment.kt
@@ -0,0 +1,25 @@
+package com.udacity.shoestore.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(
+            inflater, R.layout.fragment_login, container, false
+        )
+        return binding.root
+    }
+
+}
